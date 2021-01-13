3e227ec1a1da7f387dc1c50fec2987dec0a99c9a
EzequielRT <ezefuts@gmail.com>
2021-01-12 22:12:56 -0300

    Correted Footer

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -6,7 +6,7 @@ import { ReactComponent as InstagramIcon } from './youtube.svg';
 function Footer() {
     return (
         <footer className="main-footer">
-            App desenvolvido por <strong>Ezequiel Rodrigues<strong>
+            App desenvolvido por <strong>Ezequiel Rodrigues</strong>
             <div className="footer-icons">
                 <a href="https://www.youtube.com/c/DevSuperior" target="_new">
                     <YoutubeIcon />
